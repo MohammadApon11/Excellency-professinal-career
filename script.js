e5a110a6f3c24104d61cd59c7dfba28e66179889
moahammadApon <mohammadapon11.com>
2023-12-31 12:51:37 +0600

    class and Object.creatte() learned

--- script.js
+++ script.js
@@ -72,49 +72,49 @@
 
 // this keyword
 
-// 1. Global => this = global object (Incase of browser it's Window object)
-console.log(this === window); // output: In a browser environment, this would be true
+// // 1. Global => this = global object (Incase of browser it's Window object)
+// console.log(this === window); // output: In a browser environment, this would be true
 
-// 2. Simple function call => this = could be undefined or Global object
-// "use strict";
-function test1() {
-  console.log(this); //output: if we use "use strict" undefined rather Global object
-}
-test1();
+// // 2. Simple function call => this = could be undefined or Global object
+// // "use strict";
+// function test1() {
+//   console.log(this); //output: if we use "use strict" undefined rather Global object
+// }
+// test1();
 
-// 3. Arrow function => this = Doesn't have "this" of it's own (Lexical this). it have lexical this
+// // 3. Arrow function => this = Doesn't have "this" of it's own (Lexical this). it have lexical this
 
-const test2 = () => {
-  console.log(this); // output: window
-};
-test2();
+// const test2 = () => {
+//   console.log(this); // output: window
+// };
+// test2();
 
-// Method => this = The  object that is calling the method
+// // Method => this = The  object that is calling the method
 
-const pastFriends = {
-  international: 2,
-  national: 100,
-  sub: function () {
-    console.log(this.international + this.national); // output: 102
-    console.log(this); // output: pastFriends full object with sub function
-  },
-};
+// const pastFriends = {
+//   international: 2,
+//   national: 100,
+//   sub: function () {
+//     console.log(this.international + this.national); // output: 102
+//     console.log(this); // output: pastFriends full object with sub function
+//   },
+// };
 
-pastFriends.sub();
+// pastFriends.sub();
 
-const nowFriends = {
-  international: 20,
-  national: 1000,
-  // sub: function () { output: 1020}
-};
+// const nowFriends = {
+//   international: 20,
+//   national: 1000,
+//   // sub: function () { output: 1020}
+// };
 
-nowFriends.sub = pastFriends.sub;
+// nowFriends.sub = pastFriends.sub;
 
-nowFriends.sub();
+// nowFriends.sub();
 
-// Event Listener => this = DOM element
+// // Event Listener => this = DOM element
 
-const button = document.getElementById("btn");
-button.addEventListener("click", function () {
-  console.log(this);
-});
+// const button = document.getElementById("btn");
+// button.addEventListener("click", function () {
+//   console.log(this);
+// });
--- test1.js
+++ test1.js
@@ -23,100 +23,111 @@
 //   john.greet(); // Output: Hello, my name is apon
 
 // my first constructor function
-const Man = function (name, hand, leg, eye, religion) {
-  (this.name = name),
-    (this.hand = hand),
-    (this.leg = leg),
-    (this.eye = eye),
-    (this.religion = religion);
-};
+// const Man = function (name, hand, leg, eye, religion) {
+//   (this.name = name),
+//     (this.hand = hand),
+//     (this.leg = leg),
+//     (this.eye = eye),
+//     (this.religion = religion);
+// };
 
 //* step 1. create a object {} the "new" keyword
 //* step 2. Function is called and, this = {}
 //* step 3. Empty object is linked to prototype
 //* step 4. Object is returned
 
-const apon = new Man("Apon", 2, 2, 2, "Islam");
-const mitun = new Man("Mithun", 2, 2, 2, "Hindu");
+// const apon = new Man("Apon", 2, 2, 2, "Islam");
+// const mitun = new Man("Mithun", 2, 2, 2, "Hindu");
 
-Man.prototype.nameAndReligion = function () {
-  return this.name + " " + this.religion;
-};
-
-console.log(apon); // output: Man {name: 'Apon', hand: 2, leg: 2, eye: 2, religion: 'Islam'}
-console.log(mitun); // output: Man {name: 'Mithun', hand: 2, leg: 2, eye: 2, religion: 'Hindu'}
-
-console.log(Man.prototype); // output: {nameAndReligion: ƒ, constructor: ƒ}
-console.log(apon.__proto__); // output {nameAndReligion: ƒ, constructor: ƒ}
-
-console.log(apon.nameAndReligion()); // output: Apon Islam
-console.log(mitun.nameAndReligion()); // output: Mithun Hindu
-
-const arr = [1, 2, 3, 3, 4, 45, 5];
-const obj = { name: "Apon" };
-console.log(arr.__proto__); //output: [constructor: ƒ, at: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, …]
-console.log(obj.__proto__); // output: {constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, …}
-
-
-// MCQ Test
-
-// console.log("Task 1");
-// setTimeout(() => {
-//  console.log("Task 2");
-// }, 5000);
-// console.log("Task 3");
-// setTimeout(() => {
-//  console.log("Task 4");
-// }, 0);
-// console.log("Task 5");
-
-// "use strict";
-
-// const func = () => {
-//  console.log(this);
+// Man.prototype.nameAndReligion = function () {
+//   return this.name + " " + this.religion;
 // };
-// func()
 
-// var name = "Hero";
-// function changeName() {
-//  console.log(name);
-//  var name = "Alam";
+// console.log(apon); // output: Man {name: 'Apon', hand: 2, leg: 2, eye: 2, religion: 'Islam'}
+// console.log(mitun); // output: Man {name: 'Mithun', hand: 2, leg: 2, eye: 2, religion: 'Hindu'}
+
+// console.log(Man.prototype); // output: {nameAndReligion: ƒ, constructor: ƒ}
+// console.log(apon.__proto__); // output {nameAndReligion: ƒ, constructor: ƒ}
+
+// console.log(apon.nameAndReligion()); // output: Apon Islam
+// console.log(mitun.nameAndReligion()); // output: Mithun Hindu
+
+// const arr = [1, 2, 3, 3, 4, 45, 5];
+// const obj = { name: "Apon" };
+// console.log(arr.__proto__); //output: [constructor: ƒ, at: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, …]
+// console.log(obj.__proto__); // output: {constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, …}
+
+//* class decralation
+// class Man{
+
 // }
-// changeName()
 
-// const count = () => {
-//     for (var i = 1; i < 5; i++) {}
-//     console.log(i);
-//    };
-//    count();
+//* class expression
+// const Man = class {
+//   constructor(name, age, religion) {
+//     this.name = name;
+//     this.age = age;
+//     this.religion = religion;
+//   }
+//   // instance method
+//   addTwo() {
+//     return this.name + this.religion;
+//   }
 
-//    const car = {
-//     imported: 250,
-//     price: 26500000,
-//     calc: function () {
-//      console.log(this.price * this.imported);
-//     },
-//    };
-//    const bike = {
-//     imported: 150,
-//     price: 2200000,
-//    };
-   
-// //    bike.calc();
+//   // static method
+//   static Hello(name, grade) {
+//     return name + " SCC Result" + grade;
+//   }
+//   static multiply(x, y) {
+//     return x * y;
+//   }
+// };
 
-//    const b = 5;
-// function calc(num1, num2) {
-//  const a = num1;
-//  const b = 2;
-//  function sum() {
-//   return a + b;
-//  }
-//  function sub() {
-//   return a - b;
-//  }
-//  var summationResult = sum();
-//  var subtractionResult = sub();
-//  return { summationResult, subtractionResult };
-// }
-// const result = calc(5, 15);
-// console.log(result);
+// const Man1 = new Man("apon", 24, "islam");
+// const Man2 = new Man("mithin", 24, "hindu");
+// console.log(Man1.addTwo());
+// console.log(Man2.__proto__);
+// //* static function call
+// Man.Hello("apon", 4.59);
+// const product = Man.multiply(4, 6); // Calling the 'multiply' static method
+// console.log('Product:', product); // Output: Product: 24
+
+// Object.create();
+
+// example 1
+// const ManProto = {
+//   ManAbout() {
+//     this.name + " " + this.religion;
+//   },
+//   ManDetail(name, religion, fathersName) {
+//     this.name = name;
+//     this.religion = religion;
+//     this.fathersName = fathersName;
+//   },
+// };
+
+// const Man1 = Object.create(ManProto);
+// Man1.ManDetail("apon", "islam", "late mukhlesur rahman");
+// console.log(Man1);
+
+// example 2
+// Prototype object
+// const Animal = {
+//   makeSound() {
+//     return `${this.sound}!`;
+//   },
+//   setSpecies(species) {
+//     this.species = species;
+//   },
+// };
+
+// // Creating a new object using Object.create() with 'Animal' as its prototype
+// const dog = Object.create(Animal);
+
+// // Adding specific properties to the 'dog' object
+// dog.sound = 'Woof';
+// dog.setSpecies('Canine');
+
+// // Using methods and properties of 'dog'
+// console.log(dog.makeSound()); // Output: Woof!
+// console.log(dog.species); // Output: Canine
